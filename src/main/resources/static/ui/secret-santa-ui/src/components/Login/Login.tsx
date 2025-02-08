9567ed7347570706ec6b8a6d18cb5715a08408ec
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-02-08 18:59:00 +0100

    login and registration work now

--- src/main/resources/static/ui/secret-santa-ui/src/components/Login/Login.tsx
+++ src/main/resources/static/ui/secret-santa-ui/src/components/Login/Login.tsx
@@ -1,21 +1,63 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../../assets/css/login-registration.css";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { AppDispatch, RootState } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../store/authSlice";
+
+export type LoginFormData = {
+  username: string;
+  password: string;
+};
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: {},
+  } = useForm<LoginFormData>();
+
+  const { loading, userToken } = useSelector((state: RootState) => state.auth);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userToken) {
+      navigate("/home");
+    }
+  }, [navigate, userToken]);
+
+  const onSubmit = (data: LoginFormData) => {
+    dispatch(login(data));
+  };
+
   return (
     <div className="full-page-container">
       <div className="centered-form-container">
         <h1 className="title">Log in</h1>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3">
-        
-            <Form.Control type="text" placeholder="Username" />
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              {...register("username")}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password")}
+            />
           </Form.Group>
-          <Button variant="primary" type="submit">Log in</Button>
+          <Button variant="primary" type="submit">
+            {loading ? "Loading" : "Log in"}
+          </Button>
         </Form>
       </div>
     </div>
--- src/main/resources/static/ui/secret-santa-ui/src/components/Register/Register.tsx
+++ src/main/resources/static/ui/secret-santa-ui/src/components/Register/Register.tsx
@@ -1,29 +1,79 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../../assets/css/login-registration.css";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { registerUser } from "../../store/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export type RegisterFormData = {
+  name: string;
+  surname: string;
+  username: string;
+  password: string;
+};
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: {},
+  } = useForm<RegisterFormData>();
+
+  const { loading, userToken, success } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (success) navigate("/login");
+    if (userToken) navigate("/home");
+  }, [navigate, userToken, success]);
+
+  const onSubmit = (data: RegisterFormData) => {
+    dispatch(registerUser(data));
+  };
+
   return (
     <div className="full-page-container">
       <div className="centered-form-container">
         <h1 className="title">Register</h1>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3 d-flex">
-           
-            <Form.Control type="text" placeholder="Name" className="me-2" />
-            
-            <Form.Control type="text" placeholder="Surname" />
+            <Form.Control
+              type="text"
+              placeholder="Name"
+              className="me-2"
+              {...register("name")}
+            />
+
+            <Form.Control
+              type="text"
+              placeholder="Surname"
+              {...register("surname")}
+            />
           </div>
           <Form.Group className="mb-3">
-        
-            <Form.Control type="text" placeholder="Username" />
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              {...register("username")}
+            />
           </Form.Group>
-          <Form.Group className="mb-3" >
-          
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password")}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Register
+            {loading ? "Loading" : "Register"}
           </Button>
         </Form>
       </div>
