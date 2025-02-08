226f1d82f5c8bebcbae14f96c384f53ad608655b
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-02-08 16:56:13 +0100

    added all the routes to App.tsx

--- src/main/resources/static/ui/secret-santa-ui/src/App.tsx
+++ src/main/resources/static/ui/secret-santa-ui/src/App.tsx
@@ -1,11 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "./components/NavigationBar";
+import { LoginPage, HomePage, RegisterPage, DashboardPage } from "./pages";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./components/NotFound";
 
 function App() {
-
   return (
     <>
-      
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
