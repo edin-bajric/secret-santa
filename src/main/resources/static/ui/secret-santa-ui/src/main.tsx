c7cfab915f2de544590d3e1d78e8ed69366efd05
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-02-08 19:45:11 +0100

    added queryClient

--- src/main/resources/static/ui/secret-santa-ui/src/main.tsx
+++ src/main/resources/static/ui/secret-santa-ui/src/main.tsx
@@ -4,13 +4,24 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
